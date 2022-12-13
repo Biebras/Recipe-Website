5d87655c7b6ca886e1b13567e9f9a81f4703e97d
sc21sv <sauliusvince@gmail.com>
2022-12-13 23:51:39 +0000

    Finnished logging

--- Cw2/app/views.py
+++ Cw2/app/views.py
@@ -1,7 +1,7 @@
 # import necessary labraries 
 from flask import render_template, request, redirect, flash
 from app import app, db, login_manager
-from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_login import login_user, login_required, logout_user, current_user
 from .forms import RegisterForm, LoginForm, RecipeForm, ProfileForm
 from .models import UserModel, RecipeModel
 
@@ -130,27 +130,34 @@ def profile():
 
 @app.route("/allRecipies", methods=["POST", "GET"])
 def allRecipies():
+    app.logger.info("all recipies route request")
     allRecipies = RecipeModel.query.all()
+    app.logger.info("rendering all recipies template")
     return render_template("allRecipies.html", title="All Recipes Page", user = current_user, allRecipies = allRecipies)
 
 @app.route("/yourRecipies", methods=["POST", "GET"])
 @login_required
 def yourRecipies():
+    app.logger.info("your recipies route request")
+    app.logger.info("rendering your recipies template")
     return render_template("yourRecipies.html", title="All Recipes Page", user = current_user, yourRecipies = current_user.recipes)
 
 @app.route("/favoritesRecipies", methods=["POST", "GET"])
 @login_required
 def favoriteRecipies():
+    app.logger.info("favorite recipies route request")
+    app.logger.info("rendering favorite recipies template")
     return render_template("favoriteRecipies.html", title="All Recipes Page", user = current_user, favoriteRecipies = current_user.favorites)
 
 @app.route("/addRecipe", methods=["POST", "GET"])
 @login_required
 def addRecipe():
+    app.logger.info("add recipies route request")
     #Create form
     form = RecipeForm(request.form)
 
-    if(request.method == 'POST' and form.validate() == False):
-        print("Hmm?")
+    if(request.method == 'POST' and form.validate()):
+        app.logger.info("add recipe form validate success")
         ingrediants = ""
         for x in form.ingrediants:
             ingrediants += "{" + x.ingrediant.data + "|" + x.quantity.data + "}"
@@ -158,41 +165,54 @@ def addRecipe():
         recipe = RecipeModel(form.name.data, ingrediants, form.instructions.data, form.image_url.data, current_user.id)
         db.session.add(recipe)
         db.session.commit()
+        app.logger.info("recipe commited to database, refreshing page to index")
         return redirect("/")
 
+    app.logger.info("rendering add recipe template")
     return render_template("addRecipe.html", title="Add Recipe Page", user = current_user, form = form)
 
 @app.route("/recipe/", methods=["POST", "GET"])
 def recipe():
+    app.logger.info("recipe route request")
     recipe = get_current_recipe()
     followers_count = len(recipe.followers)
+    app.logger.info("rendering recipe template")
     return render_template("recipe.html", title="Recipe Page", user = current_user, current_recipe = recipe, followers_count = followers_count)
 
 @app.route("/findRecipe/<int:id>", methods=["POST", "GET"])
 def findRecipe(id):
+    app.logger.info("find recipe function call")
     global current_recipe_id
     current_recipe_id = id
+    app.logger.info("rederecting to recipe template")
     return redirect("/recipe")
 
 @app.route("/followRecipe", methods=["POST", "GET"])
 @login_required
 def followRecipe():
+    app.logger.info("follow recipe function call")
     recipe = get_current_recipe()
     current_user.favorites.append(recipe)
     db.session.commit()
+    app.logger.info("recipe follow data was commited")
+    app.logger.info("rederecting to recipe template")
     return redirect("/recipe/")
 
 @app.route("/unfollowRecipe", methods=["POST", "GET"])
 @login_required
 def unfollowRecipe():
+    app.logger.info("unfollow recipe function call")
     recipe = get_current_recipe()
     current_user.favorites.remove(recipe)
     db.session.commit()
+    app.logger.info("recipe unfollow data was commited")
+    app.logger.info("rederecting to recipe template")
     return redirect("/recipe")
 
 @app.route("/deleteRecipe", methods=["POST", "GET"])
 @login_required
 def deleteRecipe():
+    app.logger.info("delete recupe function call")
     recipe = get_current_recipe()
 
     if(recipe.owner.id != current_user.id):
@@ -200,6 +220,8 @@ def deleteRecipe():
 
     db.session.delete(recipe)
     db.session.commit()
+    app.logger.info("delete recipe, changes commited")
+    app.logger.info("rederecting to index template")
     return redirect("/")
 
 @app.after_request
